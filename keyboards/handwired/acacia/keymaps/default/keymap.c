a00fc730ec63bcb9f95ee378db90d0779f431595
Jens Peter Secher <jpsecher@gmail.com>
2023-01-17 09:00:20 +0100

    Tab and Enter on thumb tab

--- keyboards/handwired/acacia/keymaps/default/keymap.c
+++ keyboards/handwired/acacia/keymaps/default/keymap.c
@@ -32,6 +32,8 @@
 #define SPC_GUI LGUI_T(KC_SPC)
 #define NUL_GUI LGUI_T(KC_0)
 #define OSM_AGR OSM(MOD_RALT)
+#define TAB_ALT LALT_T(KC_TAB)
+#define ENT_CTL LCTL_T(KC_ENT)
 
 enum layer_names {
     _QWERTY,
@@ -44,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_K,    KC_Q,    KC_W,    KC_D,    KC_R,    KC_G,       KC_H,    KC_U,    KC_I,    KC_O,    KC_P,    KC_Y,
     KC_TAB,  KC_A,    KC_S,    KC_T,    KC_F,    KC_B,       KC_N,    KC_J,    KC_E,    KC_L,    KC_SCLN, KC_ENT,
              KC_Z,    KC_X,    KC_C,    KC_V,                         KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
-             KC_MNXT, DEL_GUI, BS_SYM,  OSM_SFT, OSM_ALT,    OSM_ALT, OSM_CTL, SPC_NAV, ESC_AGR, KC_MPLY, DB_TOGG
+             KC_MNXT, DEL_GUI, BS_SYM,  OSM_SFT, TAB_ALT,    TAB_ALT, ENT_CTL, SPC_NAV, ESC_AGR, KC_MPLY, DB_TOGG
   ),
   [_NAVI_FN] = LAYOUT(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
