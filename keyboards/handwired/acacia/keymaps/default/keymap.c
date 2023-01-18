d8cfd0db464e32cc6f14138b0040fe401b70dc74
Jens Peter Secher <jpsecher@gmail.com>
2023-01-18 14:18:26 +0100

    WIP: home row mods

--- keyboards/handwired/acacia/keymaps/default/keymap.c
+++ keyboards/handwired/acacia/keymaps/default/keymap.c
@@ -17,12 +17,20 @@
 
 #define SPC_CTL LCTL_T(KC_SPC)
 #define DEL_GUI LGUI_T(KC_DEL)
-#define DEL_CTL LCTL_T(KC_SPC)
+#define A_GUI LGUI_T(KC_A)
+#define SCLN_GUI LGUI_T(KC_SCLN)
+#define DEL_CTL LCTL_T(KC_DEL)
+#define F_CTL LCTL_T(KC_F)
+#define J_CTL LCTL_T(KC_J)
 #define OSM_SFT OSM(MOD_LSFT)
 #define OSM_ALT OSM(MOD_LALT)
 #define OSM_CTL OSM(MOD_LCTL)
 #define ESC_CTL LCTL_T(KC_ESC)
 #define ESC_AGR RALT_T(KC_ESC)
+#define S_AGR RALT_T(KC_S)
+#define L_AGR RALT_T(KC_L)
+#define T_ALT LALT_T(KC_T)
+#define E_ALT LALT_T(KC_E)
 #define BS_GUI LGUI_T(KC_BSPC)
 #define BS_CTL LCTL_T(KC_BSPC)
 #define BS_SYM LT(_SYM_NUM, KC_BSPC)
@@ -46,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_K,    KC_Q,    KC_W,    KC_D,    KC_R,    KC_G,       KC_H,    KC_U,    KC_I,    KC_O,    KC_P,    KC_Y,
     KC_TAB,  KC_A,    KC_S,    KC_T,    KC_F,    KC_B,       KC_N,    KC_J,    KC_E,    KC_L,    KC_SCLN, KC_ENT,
              KC_Z,    KC_X,    KC_C,    KC_V,                         KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
-             KC_MNXT, DEL_GUI, BS_SYM,  OSM_SFT, TAB_ALT,    TAB_ALT, ENT_CTL, SPC_NAV, ESC_AGR, KC_MPLY, DB_TOGG
+             KC_MNXT, DEL_GUI, BS_SYM,  OSM_SFT, OSM_ALT,    OSM_ALT, OSM_CTL, SPC_NAV, ESC_AGR, KC_MPLY, DB_TOGG
   ),
   [_NAVI_FN] = LAYOUT(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
