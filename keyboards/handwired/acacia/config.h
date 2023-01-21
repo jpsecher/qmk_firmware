cdb7bf14586166d60014a072d4bf55a9fefa4cf7
Jens Peter Secher <jpsecher@gmail.com>
2023-01-21 18:45:29 +0100

    Better rendering of the layout & fix URL
    - Smart caps lock when both shifts are pressed
    - Lower-row tap-mods

--- keyboards/handwired/acacia/config.h
+++ keyboards/handwired/acacia/config.h
@@ -21,3 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define TAPPING_TERM 200
 //#define PERMISSIVE_HOLD
+
+// Makes the lower-row tap-mods work better
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Smart caps lock when both shifts are pressed
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
--- keyboards/handwired/acacia/keymaps/default/keymap.c
+++ keyboards/handwired/acacia/keymaps/default/keymap.c
@@ -15,16 +15,31 @@
  */
 #include QMK_KEYBOARD_H
 
-#define SPC_CTL LCTL_T(KC_SPC)
-#define DEL_GUI LGUI_T(KC_DEL)
-#define DEL_CTL LCTL_T(KC_SPC)
-#define OSM_SFT OSM(MOD_LSFT)
-#define OSM_ALT OSM(MOD_LALT)
-#define ESC_CTL LCTL_T(KC_ESC)
-#define BS_GUI LGUI_T(KC_BSPC)
+// Thumbs
+#define OSM_LSFT OSM(MOD_LSFT)
+#define OSM_RSFT OSM(MOD_RSFT)
 #define BS_SYM LT(_SYM_NUM, KC_BSPC)
 #define SPC_NAV LT(_NAVI_FN, KC_SPC)
-#define OSM_AGR OSM(MOD_RALT)
+
+// Alpha lower home row mods
+#define Z_GUI LGUI_T(KC_Z)
+#define X_ALT LALT_T(KC_X)
+#define C_AGR RALT_T(KC_C)
+#define V_CTL LCTL_T(KC_V)
+#define M_CTL RCTL_T(KC_M)
+#define COMM_AGR RALT_T(KC_COMM)
+#define DOT_ALT LALT_T(KC_DOT)
+#define QUOT_GUI RGUI_T(KC_QUOT)
+
+// Symbols lower home row mods
+#define BSLS_GUI LGUI_T(KC_BSLS)
+#define LBRC_ALT LALT_T(KC_LBRC)
+#define RBRC_AGR RALT_T(KC_RBRC)
+#define GRV_CTL LCTL_T(KC_GRV)
+#define ONE_CTL RCTL_T(KC_1)
+#define TWO_AGR RALT_T(KC_2)
+#define THRE_ALT LALT_T(KC_3)
+#define EQL_GUI RGUI_T(KC_EQL)
 
 enum layer_names {
     _QWERTY,
@@ -34,21 +49,21 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
-    KC_B,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_H,    KC_U,    KC_I,    KC_O,    KC_P,    KC_Y,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_N,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-             KC_Z,    KC_X,    KC_C,    KC_V,                         KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
-             KC_MNXT, OSM_ALT, BS_SYM,  ESC_CTL, OSM_SFT,    OSM_AGR, DEL_GUI, SPC_NAV, OSM_ALT, KC_MPLY, DB_TOGG
+    KC_K,     KC_Q,     KC_W,     KC_D,     KC_R,     KC_G,       KC_H,     KC_U,     KC_I,     KC_O,     KC_P,     KC_Y,
+    KC_TAB,   KC_A,     KC_S,     KC_T,     KC_F,     KC_B,       KC_J,     KC_N,     KC_E,     KC_L,     KC_SCLN,  KC_ENT,
+              Z_GUI,    X_ALT,    C_AGR,    V_CTL,                          M_CTL,    COMM_AGR, DOT_ALT,  QUOT_GUI,
+              KC_MNXT,  KC_DEL,   BS_SYM,   OSM_LSFT, KC_LALT,    KC_LALT,  OSM_RSFT, SPC_NAV,  KC_ESC,   KC_MPLY,  DB_TOGG
   ),
   [_NAVI_FN] = LAYOUT(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    KC_TAB,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_TILD,    KC_GRV,  KC_LEFT, KC_UP,   KC_RGHT, KC_WREF, KC_ENT,
-             KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE,                      KC_BTN1, KC_DOWN, KC_BTN2, KC_WBAK,
-             KC_MPRV, _______, BS_GUI, _______, _______,     _______, _______, _______, _______, KC_PWR,  _______
+    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+    KC_TAB,   KC_WH_D,  KC_HOME,  KC_PGUP,  KC_END,   KC_TILD,    KC_SLSH,  KC_LEFT,  KC_UP,    KC_RGHT,  KC_WH_U,  KC_ENT,
+              KC_LGUI,  KC_LALT,  KC_PGDN,  KC_LCTL,                        KC_RCTL,  KC_DOWN,  KC_LALT,  KC_RGUI,
+              KC_MPRV,  _______,  KC_BSPC,  _______,  _______,    _______,  _______,  _______,  _______,  KC_PWR,   EE_CLR
   ),
   [_SYM_NUM] = LAYOUT(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,    KC_AMPR, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_PLUS,
-    KC_QUES, KC_SLSH, KC_LBRC, KC_LCBR, KC_LPRN, KC_PIPE,    KC_0,    KC_4,    KC_5,    KC_6,    KC_MINS, KC_EQL,
-             KC_BSLS, KC_RBRC, KC_RCBR, KC_RPRN,                      KC_1,    KC_2,    KC_3,    KC_UNDS,
-             KC_VOLD, _______, _______, _______, _______,    _______, _______, SPC_CTL, _______, KC_VOLU, _______
+    KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,    KC_BTN1,  KC_7,     KC_8,     KC_9,     KC_AMPR,  KC_ASTR,
+    KC_QUES,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_PIPE,    KC_UNDS,  KC_4,     KC_5,     KC_6,     KC_MINS,  KC_PLUS,
+              BSLS_GUI, LBRC_ALT, RBRC_AGR, GRV_CTL,                        ONE_CTL,  TWO_AGR,  THRE_ALT, EQL_GUI,
+              KC_VOLD,  KC_BTN1,  _______,  KC_BTN2,  _______,    _______,  KC_BTN2,  KC_SPC,   KC_0,     KC_VOLU,  QK_BOOT
   )
 };
