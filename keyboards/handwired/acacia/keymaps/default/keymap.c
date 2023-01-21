21dbbef0f9c7152fa90bd98329b8cc6c4f4a5b0d
Jens Peter Secher <jpsecher@gmail.com>
2023-01-21 14:01:20 +0100

    Swap J/N and all navigation on home row

--- keyboards/handwired/acacia/keymaps/default/keymap.c
+++ keyboards/handwired/acacia/keymaps/default/keymap.c
@@ -47,14 +47,14 @@
 #define U_CTL RCTL_T(KC_U)
 
 // Alpha lower home row mods
-#define Z_GUI LGUI_T(KC_Z)
-#define X_CTL LCTL_T(KC_X)
-#define C_AGR RALT_T(KC_C)
-#define V_ALT LALT_T(KC_V)
-#define M_ALT LALT_T(KC_M)
-#define COMM_AGR RALT_T(KC_COMM)
-#define DOT_CTL RCTL_T(KC_DOT)
-#define QUOT_GUI RGUI_T(KC_QUOT)
+#define Z_AGR RALT_T(KC_Z)
+#define X_GUI LGUI_T(KC_X)
+#define C_ALT LALT_T(KC_C)
+#define V_CTL LCTL_T(KC_V)
+#define M_CTL RCTL_T(KC_M)
+#define COMM_ALT LALT_T(KC_COMM)
+#define DOT_GUI RGUI_T(KC_DOT)
+#define QUOT_AGR RALT_T(KC_QUOT)
 
 // Navigation upper home row mods
 #define F2_AGR RALT_T(KC_F2)
@@ -76,14 +76,14 @@
 #define SEVN_CTL RCTL_T(KC_7)
 
 // Symbols lower home row mods
-#define BSLS_GUI LGUI_T(KC_BSLS)
-#define LBRC_CTL LCTL_T(KC_LBRC)
-#define RBRC_AGR RALT_T(KC_RBRC)
-#define GRV_ALT LALT_T(KC_GRV)
-#define ONE_ALT LALT_T(KC_1)
-#define TWO_AGR RALT_T(KC_2)
-#define THRE_CTL RCTL_T(KC_3)
-#define EQL_GUI LGUI_T(KC_EQL)
+#define BSLS_AGR RALT_T(KC_BSLS)
+#define LBRC_GUI LGUI_T(KC_LBRC)
+#define RBRC_ALT LALT_T(KC_RBRC)
+#define GRV_CTL LCTL_T(KC_GRV)
+#define ONE_CTL RCTL_T(KC_1)
+#define TWO_ALT LALT_T(KC_2)
+#define THRE_GUI RGUI_T(KC_3)
+#define EQL_AGR RALT_T(KC_EQL)
 
 enum layer_names {
     _QWERTY,
@@ -94,20 +94,20 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_K,     KC_Q,     KC_W,     KC_D,     KC_R,     KC_G,       KC_H,     KC_U,     KC_I,     KC_O,     KC_P,     KC_Y,
-    KC_TAB,   KC_A,     KC_S,     KC_T,     KC_F,     KC_B,       KC_N,     KC_J,     KC_E,     KC_L,     KC_SCLN,  KC_ENT,
-              Z_GUI,    X_CTL,    C_AGR,    V_ALT,                          M_ALT,    COMM_AGR, DOT_CTL,  QUOT_GUI,
-              KC_MNXT,  KC_ESC,   BS_SYM,   OSM_LSFT, KC_LALT,    KC_LALT,  OSM_RSFT, SPC_NAV,  KC_DEL,   KC_MPLY,  DB_TOGG
+    KC_TAB,   KC_A,     KC_S,     KC_T,     KC_F,     KC_B,       KC_J,     KC_N,     KC_E,     KC_L,     KC_SCLN,  KC_ENT,
+              Z_AGR,    X_GUI,    C_ALT,    V_CTL,                          M_CTL,    COMM_ALT, DOT_GUI,  QUOT_AGR,
+              KC_MNXT,  KC_DEL,   BS_SYM,   OSM_LSFT, KC_LALT,    KC_LALT,  OSM_RSFT, SPC_NAV,  KC_ESC,   KC_MPLY,  DB_TOGG
   ),
   [_NAVI_FN] = LAYOUT(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
-    OSM_LSFT, KC_WH_U,  KC_HOME,  KC_PGUP,  KC_END,   KC_SLSH,    KC_TILD,  KC_LEFT,  KC_UP,    KC_RGHT,  KC_WH_D,  OSM_RSFT,
-              KC_LGUI,  KC_LCTL,  KC_PGDN,  KC_LALT,                        KC_LALT,  KC_DOWN,  KC_RCTL,  KC_RGUI,
+    KC_TAB,   KC_HOME,  KC_PGUP,  KC_PGDN,  KC_END,   KC_TILD,    KC_SLSH,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_ENT,
+              KC_RALT,  KC_LGUI,  KC_LALT,  KC_LCTL,                        KC_RCTL,  KC_LALT,  KC_RGUI,  KC_RALT,
               KC_MPRV,  _______,  KC_BSPC,  _______,  _______,    _______,  _______,  _______,  _______,  KC_PWR,   EE_CLR
   ),
   [_SYM_NUM] = LAYOUT(
-    KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,    KC_BTN1,  KC_7,     KC_8,     KC_9,     KC_UNDS,  KC_ASTR,
-    KC_QUES,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_PIPE,    KC_AMPR,  KC_4,     KC_5,     KC_6,     KC_MINS,  KC_PLUS,
-              BSLS_GUI, LBRC_CTL, RBRC_AGR, GRV_ALT,                        ONE_ALT,  TWO_AGR,  THRE_CTL, EQL_GUI,
+    KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,    KC_BTN1,  KC_7,     KC_8,     KC_9,     KC_AMPR,  KC_ASTR,
+    KC_QUES,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_PIPE,    KC_UNDS,  KC_4,     KC_5,     KC_6,     KC_MINS,  KC_PLUS,
+              BSLS_AGR, LBRC_GUI, RBRC_ALT, GRV_CTL,                        ONE_CTL,  TWO_ALT,  THRE_GUI, EQL_AGR,
               KC_VOLD,  KC_BTN1,  _______,  KC_BTN2,  _______,    _______,  KC_BTN2,  KC_SPC,   KC_0,     KC_VOLU,  QK_BOOT
   )
 };
