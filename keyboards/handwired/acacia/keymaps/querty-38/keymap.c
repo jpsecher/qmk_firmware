130a28803824428ac1a9e91d6650cde5f1ca6b03
Jens Peter Secher <jpsecher@gmail.com>
2023-10-01 01:08:50 +0200

    Use Enter + Shift as Caps

--- keyboards/handwired/acacia/keymaps/querty-38/keymap.c
+++ keyboards/handwired/acacia/keymaps/querty-38/keymap.c
@@ -88,7 +88,7 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA] = LAYOUT(
     XXXXXXX,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,       KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     XXXXXXX,
-    KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     G_AGR,      H_AGR,    KC_J,     KC_K,     KC_L,     KC_N,     KC_ENT,
+    KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     G_AGR,      H_AGR,    KC_J,     KC_K,     KC_L,     KC_N,     SC_SENT,
               Z_GUI,    X_ALT,    C_SYM,    V_CTL,                          M_CTL,    COMM_SYM, DOT_ALT,  B_GUI,
               KC_MNXT,  LOCK_NAV, OSM_LSFT, LOCK_ALP, XXXXXXX,    XXXXXXX,  QK_GESC,  SPC_FUNC, KC_BSPC,  KC_MPLY,  DB_TOGG
   ),
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,  CTLQ,     CTLW,     CTLE,     CTLR,     CTLT,       CTLY,     KC_F7,    KC_F8,    KC_F9,    KC_F11,   XXXXXXX,
     CTLTAB,   CTLA,     CTLS,     CTLD,     CTLF,     CTLG,       CTLH,     KC_F4,    KC_F5,    KC_F6,    KC_F12,   CTLENT,
               KC_LGUI,  KC_LALT,  CTLK,     KC_LCTL,                        KC_F1,    KC_F2,    KC_F3,    CTLB,
-              KC_MPRV,  ALTO,     CW_TOGG,  CTLO,     XXXXXXX,    XXXXXXX,  KC_PWR,   _______,  KC_F10,   KC_MNXT,  XXXXXXX
+              KC_MPRV,  ALTO,     _______,  CTLO,     XXXXXXX,    XXXXXXX,  KC_PWR,   _______,  KC_F10,   KC_MNXT,  XXXXXXX
   )
 };
 
