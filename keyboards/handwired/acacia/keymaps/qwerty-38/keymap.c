91b52d086459c175bce2d19701569a8ea8836a77
Jens Peter Secher <jpsecher@gmail.com>
2023-11-14 09:51:20 +0100

    Ctrl on outer keys also

--- keyboards/handwired/acacia/keymaps/qwerty-38/keymap.c
+++ keyboards/handwired/acacia/keymaps/qwerty-38/keymap.c
@@ -70,9 +70,12 @@ enum layer_names {
 #define CTLG LCTL(KC_G)
 #define CTLH LCTL(KC_H)
 #define CTLTAB LCTL(KC_TAB)
+#define CTLEQL LCTL(KC_EQL)
 #define ALTTAB LALT(KC_TAB)
 #define GUITAB LGUI(KC_TAB)
-#define CTLENT LCTL(KC_ENT)
+#define ENT_CTL RCTL_T(KC_ENT)
+#define TAB_CTL LCTL_T(KC_TAB)
+#define SLSH_CTL LCTL_T(KC_SLSH)
 #define CTLB LCTL(KC_B)
 #define ALTO LALT(KC_O)
 #define PGDN_CTL RCTL_T(KC_PGDN)
@@ -90,33 +93,33 @@ enum layer_names {
 #define ONE_CTL RCTL_T(KC_1)
 #define TWO_SYM LT(_SYM_NUM, KC_2)
 #define THRE_ALT LALT_T(KC_3)
-#define EQL_GUI RGUI_T(KC_EQL)
+#define EQL_CTL RCTL_T(KC_EQL)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA] = LAYOUT(
-    XXXXXXX,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,       KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     XXXXXXX,
-    KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,       KC_H,     KC_J,     KC_K,     KC_L,     KC_B,     KC_ENT,
-              Z_GUI,    X_ALT,    C_SYM,    V_CTL,                          N_CTL,    M_SYM,    COMM_ALT, DOT_GUI,
-              KC_MNXT,  OSM_AGR,  OSM_LSFT, TT_NAV,   XXXXXXX,    XXXXXXX,  KC_ESC,   SPC_FUNC, KC_BSPC,  KC_MPLY,  DB_TOGG
+    XXXXXXX,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     XXXXXXX,
+    TAB_CTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_B,     ENT_CTL,
+              Z_GUI,    X_ALT,    C_SYM,    V_CTL,                         N_CTL,    M_SYM,    COMM_ALT, DOT_GUI,
+              KC_MNXT,  OSM_AGR,  OSM_LSFT, TT_NAV,   XXXXXXX,   XXXXXXX,  KC_ESC,   SPC_FUNC, KC_BSPC,  KC_MPLY,  DB_TOGG
   ),
   [_NAVI] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_ACL0,  KC_ACL1,  KC_BTN1,    KC_HOME,  KC_WH_D,  KC_WH_U,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    _______,  CTLZ,     CTLX,     CTLC,     CTLV,     KC_BTN2,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_END,   _______,
-              KC_LGUI,  KC_LALT,  MO_SYM,   KC_LCTL,                        PGDN_CTL, PGUP_SYM, KC_LALT,  KC_LGUI,
-              KC_MPRV,  _______,  _______,  _______,  XXXXXXX,    XXXXXXX,  LOCK_ALP, _______,  _______,  KC_MPLY,  EE_CLR
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_ACL0,  KC_ACL1,  KC_BTN1,   KC_HOME,  KC_WH_D,  KC_WH_U,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    _______,  CTLZ,     CTLX,     CTLC,     CTLV,     KC_BTN2,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_END,   _______,
+              KC_LGUI,  KC_LALT,  MO_SYM,   KC_LCTL,                       PGDN_CTL, PGUP_SYM, KC_LALT,  KC_RGUI,
+              KC_MPRV,  _______,  _______,  _______,  XXXXXXX,   XXXXXXX,  LOCK_ALP, _______,  _______,  KC_MPLY,  EE_CLR
   ),
   [_SYM_NUM] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  KC_LCBR,  KC_DQUO,  KC_RCBR,  XXXXXXX,    KC_BSLS,  KC_7,     KC_8,     KC_9,     KC_GRV,   XXXXXXX,
-    GUITAB,   KC_COLN,  KC_LBRC,  KC_QUOT,  KC_RBRC,  XXXXXXX,    KC_COMM,  KC_4,     KC_5,     KC_6,     KC_MINS,  KC_EQL,
-              KC_LGUI,  KC_LALT,  SCLN_SYM, KC_LCTL,                        ONE_CTL,  TWO_SYM,  THRE_ALT, SLSH_GUI,
-              KC_VOLD,  KC_LT,    _______,  KC_GT,    XXXXXXX,    XXXXXXX,  KC_DOT,   _______,  KC_0,     KC_VOLU,  QK_BOOT
+    XXXXXXX,  XXXXXXX,  KC_LCBR,  KC_DQUO,  KC_RCBR,  XXXXXXX,   KC_BSLS,  KC_7,     KC_8,     KC_9,     KC_GRV,   XXXXXXX,
+    SLSH_CTL, KC_COLN,  KC_LBRC,  KC_QUOT,  KC_RBRC,  XXXXXXX,   KC_COMM,  KC_4,     KC_5,     KC_6,     KC_MINS,  EQL_CTL,
+              KC_LGUI,  KC_LALT,  SCLN_SYM, KC_LCTL,                       ONE_CTL,  TWO_SYM,  THRE_ALT, SLSH_GUI,
+              KC_VOLD,  KC_LT,    _______,  KC_GT,    XXXXXXX,   XXXXXXX,  KC_DOT,   _______,  KC_0,     KC_VOLU,  QK_BOOT
   ),
   [_FUNC] = LAYOUT(
-    XXXXXXX,  CTLQ,     CTLW,     CTLE,     CTLR,     CTLT,       CTLY,     KC_F7,    KC_F8,    KC_F9,    KC_F11,   XXXXXXX,
-    CTLTAB,   CTLA,     CTLS,     CTLD,     CTLF,     CTLG,       CTLH,     KC_F4,    KC_F5,    KC_F6,    KC_F12,   CTLENT,
-              KC_LGUI,  KC_LALT,  CTLK,     KC_LCTL,                        KC_F1,    KC_F2,    KC_F3,    CTLB,
-              KC_MPRV,  CTLO,     _______,  CTLI,     XXXXXXX,    XXXXXXX,  KC_PWR,   _______,  KC_F10,   KC_MNXT,  XXXXXXX
+    XXXXXXX,  CTLQ,     CTLW,     CTLE,     CTLR,     CTLT,      CTLY,     KC_F7,    KC_F8,    KC_F9,    KC_F11,   XXXXXXX,
+    _______,  CTLA,     CTLS,     CTLD,     CTLF,     CTLG,      CTLH,     KC_F4,    KC_F5,    KC_F6,    KC_F12,   _______,
+              KC_LGUI,  KC_LALT,  CTLK,     KC_LCTL,                       KC_F1,    KC_F2,    KC_F3,    CTLB,
+              KC_MPRV,  CTLO,     _______,  CTLI,     XXXXXXX,   XXXXXXX,  KC_PWR,   _______,  KC_F10,   KC_MNXT,  XXXXXXX
   )
 };
 
