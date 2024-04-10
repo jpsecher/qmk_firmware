ca01f0ecf572269dd10accfdeb9cc7f93d27f9e2
Jens Peter Secher <jpsecher@gmail.com>
2024-04-10 09:16:51 +0200

    Enter back on alpha layer

--- keyboards/handwired/acacia_2/keymaps/qwerty-36-pc/keymap.c
+++ keyboards/handwired/acacia_2/keymaps/qwerty-36-pc/keymap.c
@@ -32,6 +32,7 @@ enum layer_names {
 #define SPC_FUNC LT(_FUNC, KC_SPC)
 #define OSM_AGR OSM(MOD_RALT)
 #define Z_GUI LGUI_T(KC_Z)
+#define B_GUI RGUI_T(KC_B)
 #define X_ALT LALT_T(KC_X)
 #define LBRC_ALT LALT_T(KC_LBRC)
 #define C_SYM LT(_SYM_NUM, KC_C)
@@ -80,19 +81,25 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA] = LAYOUT(
               XXXXXXX,  KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     XXXXXXX,
-    KC_Q,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_B,     KC_P,
-              Z_GUI,    X_ALT,    C_SYM,    V_CTL,                         N_CTL,    M_SYM,    COMM_ALT, DOT_GUI,
+    KC_Q,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_P,     KC_ENT,
+              Z_GUI,    X_ALT,    C_SYM,    V_CTL,                         N_CTL,    M_SYM,    COMM_ALT, B_GUI,
                         KC_MNXT,  ESC_AGR,  OSM_LSFT, TT_NAV,    KC_TAB,   SPC_FUNC, KC_BSPC,  KC_MPLY
   ),
   [_NAVI] = LAYOUT(
               XXXXXXX,  KC_BTN3,  KC_WH_U,  KC_WH_D,  KC_BTN2,   KC_HOME,  KC_WH_D,  KC_WH_U,  QK_REP,  XXXXXXX,
-    KC_TAB,   CTLZ,     CTLX,     CTLC,     CTLV,     KC_BTN1,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_END,   KC_ENT,
+    CTLU,     CTLZ,     CTLX,     CTLC,     CTLV,     KC_BTN1,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_END,   _______,
               KC_LGUI,  KC_LALT,  MO_SYM,   KC_LCTL,                       PGDN_CTL, PGUP_SYM, KC_LALT,  KC_RGUI,
                         _______,  LCK_ALP,  _______,  OSL_ALP,   _______,  _______,  _______,  _______
   ),
+  [_ALPHA_OL] = LAYOUT(
+              XXXXXXX,  KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     XXXXXXX,
+    KC_Q,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_P,     _______,
+              KC_Z,     KC_X,     KC_C,     KC_V,                          KC_N,     KC_M,     KC_COMM,  KC_B,
+                        KC_MNXT,  KC_ESC,   _______,  XXXXXXX,   _______,  KC_SPC,   _______,  _______
+  ),
   [_FUNC] = LAYOUT(
               XXXXXXX,  CTLW,     CTLE,     CTLR,     CTLT,      CTLY,     KC_F7,    KC_F8,    KC_F9,    XXXXXXX,
-    CTLQ,     CTLA,     CTLS,     CTLD,     CTLF,     CTLG,      CTLH,     KC_F4,    KC_F5,    KC_F6,    KC_F12,   KC_ENT,
+    CTLQ,     CTLA,     CTLS,     CTLD,     CTLF,     CTLG,      CTLH,     KC_F4,    KC_F5,    KC_F6,    KC_F12,   _______,
               KC_LGUI,  KC_LALT,  CTLK,     KC_LCTL,                       F1_CTL,   KC_F2,    F3_ALT,   F11_GUI,
                         KC_MPRV,  CTLO,     _______,  CTLI,      KC_PWR,   _______,  KC_F10,   KC_MNXT
   ),
@@ -100,13 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               XXXXXXX,  KC_LCBR,  KC_DQUO,  KC_RCBR,  KC_TILD,   KC_COMM,  KC_7,     KC_8,     KC_9,     XXXXXXX,
     KC_GRV,   KC_COLN,  KC_LPRN,  KC_QUOT,  KC_RPRN,  KC_SCLN,   KC_DOT,   KC_4,     KC_5,     KC_6,     KC_MINS,  KC_EQL,
               KC_LGUI,  LBRC_ALT, XXXXXXX,  RBRC_CTL,                      ONE_CTL,  TWO_SYM,  THRE_ALT, BSLS_GUI,
-                        KC_VOLD,  KC_LT,    _______,  KC_GT,     KC_SLSH,  _______,  KC_0,     KC_VOLU
-  ),
-  [_ALPHA_OL] = LAYOUT(
-              XXXXXXX,  KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     XXXXXXX,
-    KC_Q,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_B,     KC_P,
-              KC_Z,     KC_X,     KC_C,     KC_V,                          KC_N,     KC_M,     KC_COMM,  KC_DOT,
-                        KC_MNXT,  KC_ESC,   _______,  TT_NAV,    KC_TAB,   _______,  KC_BSPC,  KC_MPLY
+                        KC_VOLD,  KC_LT,    _______,  KC_GT,     KC_SLSH,  KC_SPC,   KC_0,     KC_VOLU
   )
 };
 
