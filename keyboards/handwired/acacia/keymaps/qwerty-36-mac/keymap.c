c8d7921e8e93f4ddfc54ca913e69fd8a00a9041a
Jens Peter Secher <jpsecher@gmail.com>
2024-05-11 10:45:36 +0200

    Extra dot

--- keyboards/handwired/acacia/keymaps/qwerty-36-mac/keymap.c
+++ keyboards/handwired/acacia/keymaps/qwerty-36-mac/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2023 Jens Peter Secher <jpsecher@gmail.com>
+/* Copyright 2024 Jens Peter Secher <jpsecher@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,6 +33,7 @@ enum layer_names {
 #define SPC_FUNM LT(_FUNC_MAC, KC_SPC)
 #define OSM_AGR OSM(MOD_RALT)
 #define Z_GUI LGUI_T(KC_Z)
+#define B_GUI RGUI_T(KC_B)
 #define X_ALT LALT_T(KC_X)
 #define LBRC_ALT LALT_T(KC_LBRC)
 #define C_SYM LT(_SYM_NUM, KC_C)
@@ -65,6 +66,7 @@ enum layer_names {
 #define GUIG LGUI(KC_G)
 #define GUIH LGUI(KC_H)
 #define GUIB LGUI(KC_B)
+#define GUIU LGUI(KC_U)
 #define PGDN_CTL RCTL_T(KC_PGDN)
 #define PGUP_SYM LT(_SYM_NUM, KC_PGUP)
 #define ONE_CTL RCTL_T(KC_1)
@@ -79,33 +81,33 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA_MAC] = LAYOUT(
     XXXXXXX,  XXXXXXX,  KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     XXXXXXX,  XXXXXXX,
-    KC_Q,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_B,     KC_P,
-              Z_GUI,    X_ALT,    C_SYM,    V_CTL,                         N_CTL,    M_SYM,    COMM_ALT, DOT_GUI,
+    KC_Q,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_P,     KC_ENT,
+              Z_GUI,    X_ALT,    C_SYM,    V_CTL,                         N_CTL,    M_SYM,    COMM_ALT, B_GUI,
               KC_MNXT,  ESC_AGR,  OSM_LSFT, TT_NAVM,  XXXXXXX,   XXXXXXX,  KC_TAB,   SPC_FUNM, KC_BSPC,  KC_MPLY,  DB_TOGG
   ),
   [_NAVI_MAC] = LAYOUT(
     XXXXXXX,  XXXXXXX,  KC_BTN3,  KC_WH_D,  KC_WH_U,  KC_BTN2,   KC_HOME,  KC_WH_U,  KC_WH_D,  QK_REP,   XXXXXXX,  XXXXXXX,
-    KC_TAB,   GUIZ,     GUIX,     GUIC,     GUIV,     KC_BTN1,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_END,   KC_ENT,
+    GUIU,     GUIZ,     GUIX,     GUIC,     GUIV,     KC_BTN1,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_END,   _______,
               KC_LGUI,  KC_LALT,  MO_SYM,   KC_LCTL,                       PGDN_CTL, PGUP_SYM, KC_LALT,  KC_RGUI,
               _______,  LCK_ALPM, _______,  OSL_ALPM, XXXXXXX,   XXXXXXX,  _______,  _______,  _______,  _______,  EE_CLR
   ),
+  [_ALPHA_OL] = LAYOUT(
+    XXXXXXX,  XXXXXXX,  KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     XXXXXXX,  XXXXXXX,
+    KC_Q,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_P,     _______,
+              KC_Z,     KC_X,     KC_C,     KC_V,                          KC_N,     KC_M,     KC_COMM,  KC_B,
+              _______,  KC_ESC,   _______,  XXXXXXX,  XXXXXXX,   XXXXXXX,  _______,  KC_SPC,   KC_BSPC,  KC_MPLY,  DB_TOGG
+  ),
   [_FUNC_MAC] = LAYOUT(
     XXXXXXX,  XXXXXXX,  GUIW,     GUIE,     GUIR,     GUIT,      GUIY,     KC_F7,    KC_F8,    KC_F9,    XXXXXXX,  XXXXXXX,
-    GUIQ,     GUIA,     GUIS,     GUID,     GUIF,     GUIG,      GUIH,     KC_F4,    KC_F5,    KC_F6,    KC_F12,   KC_ENT,
+    GUIQ,     GUIA,     GUIS,     GUID,     GUIF,     GUIG,      GUIH,     KC_F4,    KC_F5,    KC_F6,    KC_F12,   _______,
               KC_LGUI,  KC_LALT,  GUIK,     KC_LCTL,                       F1_CTL,   KC_F2,    F3_ALT,   F11_GUI,
               KC_MPRV,  GUIO,     _______,  GUII,     XXXXXXX,   XXXXXXX,  KC_PWR,   _______,  KC_F10,   KC_MNXT,  XXXXXXX
   ),
   [_SYM_NUM] = LAYOUT(
     XXXXXXX,  XXXXXXX,  KC_LCBR,  KC_DQUO,  KC_RCBR,  KC_TILD,   KC_COMM,  KC_7,     KC_8,     KC_9,     XXXXXXX,  XXXXXXX,
     KC_GRV,   KC_COLN,  KC_LPRN,  KC_QUOT,  KC_RPRN,  KC_SCLN,   KC_DOT,   KC_4,     KC_5,     KC_6,     KC_MINS,  KC_EQL,
-              KC_LGUI,  LBRC_ALT, XXXXXXX,  RBRC_CTL,                      ONE_CTL,  TWO_SYM,  THRE_ALT, BSLS_GUI,
-              KC_VOLD,  KC_LT,    _______,  KC_GT,    XXXXXXX,   XXXXXXX,  KC_SLSH,  _______,  KC_0,     KC_VOLU,  QK_BOOT
-  ),
-  [_ALPHA_OL] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     XXXXXXX,  XXXXXXX,
-    KC_Q,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_B,     KC_P,
-              KC_Z,     KC_X,     KC_C,     KC_V,                          KC_N,     KC_M,     KC_COMM,  KC_DOT,
-              KC_MNXT,  KC_ESC,   _______,  TT_NAVM,  XXXXXXX,   XXXXXXX,  KC_TAB,   _______,  KC_BSPC,  KC_MPLY,  DB_TOGG
+              KC_LGUI,  LBRC_ALT, KC_DOT,   RBRC_CTL,                      ONE_CTL,  TWO_SYM,  THRE_ALT, BSLS_GUI,
+              KC_VOLD,  KC_LT,    _______,  KC_GT,    XXXXXXX,   XXXXXXX,  KC_SLSH,  KC_0,     KC_BSPC,  KC_VOLU,  QK_BOOT
   )  
 };
 
--- keyboards/handwired/acacia_2/keymaps/qwerty-36-pc/keymap.c
+++ keyboards/handwired/acacia_2/keymaps/qwerty-36-pc/keymap.c
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SYM_NUM] = LAYOUT(
               XXXXXXX,  KC_LCBR,  KC_DQUO,  KC_RCBR,  KC_TILD,   KC_COMM,  KC_7,     KC_8,     KC_9,     XXXXXXX,
     KC_GRV,   KC_COLN,  KC_LPRN,  KC_QUOT,  KC_RPRN,  KC_SCLN,   KC_DOT,   KC_4,     KC_5,     KC_6,     KC_MINS,  KC_EQL,
-              KC_LGUI,  LBRC_ALT, XXXXXXX,  RBRC_CTL,                      ONE_CTL,  TWO_SYM,  THRE_ALT, BSLS_GUI,
+              KC_LGUI,  LBRC_ALT, KC_DOT,   RBRC_CTL,                      ONE_CTL,  TWO_SYM,  THRE_ALT, BSLS_GUI,
                         KC_VOLD,  KC_LT,    _______,  KC_GT,     KC_SLSH,  KC_0,     KC_BSPC,  KC_VOLU
   )
 };
