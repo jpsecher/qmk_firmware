769e768427d354e8b79c0593260b64a82b51aad4
Jens Peter Secher <jpsecher@gmail.com>
2024-09-09 22:28:17 +0200

    swap 0 and dot

--- keyboards/handwired/acacia/keymaps/qwerty-34-mac/keymap.c
+++ keyboards/handwired/acacia/keymaps/qwerty-34-mac/keymap.c
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,  KC_F12,   KC_F9,    KC_F8,    KC_F7,    _______,   _______,  KC_7,     KC_8,     KC_9,     _______,  XXXXXXX,
     _______,  F11_AGR,  F6_ALT,   F5_GUI,   F4_CTL,   _______,   _______,  FOUR_CTL, FIVE_GUI, SIX_ALT,  KC_MINS,  KC_EQL,
               KC_F10,   KC_F3,    KC_F2,    KC_F1,                         KC_1,     KC_2,     KC_3,     KC_PLUS,
-              QK_BOOT,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,   XXXXXXX,  KC_COMM,  KC_DOT,   KC_0,     KC_PWR,   DB_TOGG
+              QK_BOOT,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,   XXXXXXX,  KC_COMM,  KC_0,     KC_DOT,   KC_PWR,   DB_TOGG
   ),
   [_SYM_NUM] = LAYOUT(
     XXXXXXX,  _______,  KC_LCBR,  KC_DQUO,  KC_RCBR,  KC_GRV,    KC_QUES,  KC_AMPR,  KC_ASTR,  KC_PIPE,  _______,  XXXXXXX,
