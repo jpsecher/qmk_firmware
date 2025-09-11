1d5072de45a966331f370d2b25c546e8ac0cb23f
Jens Peter Secher <jpsecher@gmail.com>
2025-09-11 10:05:25 +0200

    num layer on v/n

--- keyboards/acacia_2/keymaps/qwerty-36-mac/keymap.c
+++ keyboards/acacia_2/keymaps/qwerty-36-mac/keymap.c
@@ -140,8 +140,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALP_MAC] = LAYOUT(
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_B,
     KC_TAB,   A_AGR,    S_ALT,    D_GUI,    F_CTL,    G_NUM,     H_NUM,    J_CTL,    K_GUI,    L_ALT,    P_AGR,    KC_BSPC,
-              KC_Z,     KC_X,     KC_C,     KC_V,                          KC_N,     KC_M,     KC_COMM,  KC_DOT,
-              KC_MNXT,  XXXXXXX,  ESC_NAV, OSM_LSFT,                       OSL_SYM,  SPC_NUM,  XXXXXXX,  KC_MPLY
+              KC_Z,     KC_X,     KC_C,     V_NUM,                         N_NUM,    KC_M,     KC_COMM,  KC_DOT,
+              KC_MNXT,  XXXXXXX,  ESC_NAV,  OSM_LSFT,                      OSL_SYM,  KC_SPC,   XXXXXXX,  KC_MPLY
   ),
   [_NAV_MAC] = LAYOUT(
               OSL_ALPM, GUIY,     KC_WH_D,  KC_WH_U,  KC_BTN1,   CTLA,     KC_PGDN,  KC_PGUP,  CTLE,     QK_LLCK,
@@ -153,19 +153,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_EQL,    KC_COLN,  KC_7,     KC_8,     KC_9,     QK_LLCK,
     _______,  F5_AGR,   F6_ALT,   F7_GUI,   F8_CTL,   KC_MINS,   KC_0,     FOUR_CTL, FIVE_GUI, SIX_ALT,  KC_PLUS,  _______,
               KC_F9,    KC_F10,   KC_F11,   KC_F12,                        KC_1,     KC_2,     KC_3,     KC_DOT,
-              QK_BOOT,  _______,  LCK_ALPM, KC_SLSH,                       KC_COMM,  KC_SPC,   _______,  KC_PWR
+              QK_BOOT,  _______,  LCK_ALPM, KC_SLSH,                       KC_COMM,  _______,  _______,  KC_PWR
   ),
   [_SYMBOLS] = LAYOUT(
               OSM_HYPR, KC_LCBR,  KC_GRV,   KC_RCBR,  KC_EQL,   KC_COLN,   KC_AMPR,  KC_ASTR,  KC_QUES,  OSM_MEH,
     KC_TILD,  KC_SCLN,  KC_LBRC,  KC_DQUO,  KC_RBRC,  KC_MINS,  KC_0,      KC_DLR,   KC_PERC,  KC_CIRC,  KC_PLUS,  _______,
               KC_BSLS,  KC_LPRN,  KC_QUOT,  KC_RPRN,                       KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
-              KC_VOLD,  _______,  KC_UNDS,  KC_SLSH,                       KC_ENT,   KC_UNDS,  _______,  KC_VOLU
+              KC_VOLD,  _______,  KC_UNDS,  KC_SLSH,                       KC_ESC,   _______,  _______,  KC_VOLU
   ),
   [_ALP_OVL] = LAYOUT(
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_B,
     _______,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_P,     _______,
               KC_Z,     KC_X,     KC_C,     KC_V,                          KC_N,     KC_M,     KC_COMM,  KC_DOT,
-              _______,  _______,  LCK_ALPM, _______,                       KC_ENT,   KC_SPC,   _______,  _______
+              _______,  _______,  LCK_ALPM, _______,                       KC_ENT,   _______,  _______,  _______
   )
 };
 
@@ -205,13 +205,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case SAVE:
       if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_ESC) ":w" SS_TAP(X_ENT));
+        SEND_STRING(SS_TAP(X_ESC) ":w");
         return false;
       }
       break;
     case QUIT:
       if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_ESC) ":q" SS_TAP(X_ENT));
+        SEND_STRING(SS_TAP(X_ESC) ":q");
         return false;
       }
       break;
