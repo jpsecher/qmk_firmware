62411d4e21c376192acb75f6155d3f91c27617e9
Jens Peter Secher <jpsecher@gmail.com>
2025-12-12 11:36:18 +0100

    temp remove backspace from old posistion

--- keyboards/acacia_2/keymaps/qwerty-36-mac/keymap.c
+++ keyboards/acacia_2/keymaps/qwerty-36-mac/keymap.c
@@ -140,7 +140,7 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALP_MAC] = LAYOUT(
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
-    KC_TAB,   A_AGR,    S_ALT,    D_GUI,    F_CTL,    G_NUM,     H_NUM,    J_CTL,    K_GUI,    L_ALT,    B_AGR,    KC_BSPC,
+    KC_TAB,   A_AGR,    S_ALT,    D_GUI,    F_CTL,    G_NUM,     H_NUM,    J_CTL,    K_GUI,    L_ALT,    B_AGR,    XXXXXXX,
               KC_Z,     KC_X,     KC_C,     V_NUM,                         N_NUM,    KC_M,     KC_COMM,  KC_DOT,
               KC_MNXT,  KC_ESC,   MO_NAV,   OSM_LSFT,                      OSL_SYM,  KC_SPC,   KC_BSPC,  KC_MPLY
   ),
