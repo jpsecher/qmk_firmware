f66ba29f844c4e6c820fda2836ad58580095a286
Jens Peter Secher <jpsecher@gmail.com>
2025-12-28 14:33:26 +0100

    swap sym & bspc

--- keyboards/handwired/acacia/keymaps/qwerty-36-mac/keymap.c
+++ keyboards/handwired/acacia/keymaps/qwerty-36-mac/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     XXXXXXX,
     KC_TAB,   A_AGR,    S_ALT,    D_GUI,    F_CTL,    KC_G,      KC_H,     J_CTL,    K_GUI,    L_ALT,    B_AGR,    XXXXXXX,
               KC_Z,     KC_X,     KC_C,     V_NUM,                         N_NUM,    KC_M,     KC_COMM,  KC_DOT,
-              KC_MNXT,  KC_ESC,   MO_NAVM,  OSM_LSFT, XXXXXXX,   XXXXXXX,  KC_BSPC,  KC_SPC,   OSL_SYM,  KC_MPLY,  DB_TOGG
+              KC_MNXT,  KC_ESC,   MO_NAVM,  OSM_LSFT, XXXXXXX,   XXXXXXX,  OSL_SYM,  KC_SPC,   KC_BSPC,  KC_MPLY,  DB_TOGG
   ),
   [_NAV_MAC] = LAYOUT(
     _______,  OSL_ALP,  GUIY,     KC_WH_D,  KC_WH_U,  KC_BTN1,   CTLA,     KC_PGDN,  KC_PGUP,  CTLE,     QK_LLCK,  _______,
@@ -103,13 +103,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_EQL,    KC_COLN,  KC_7,     KC_8,     KC_9,     QK_LLCK,  _______,
     _______,  F5_AGR,   F6_ALT,   F7_GUI,   F8_CTL,   KC_MINS,   KC_0,     FOUR_CTL, FIVE_GUI, SIX_ALT,  KC_PLUS,  KC_ENT,
               KC_F9,    KC_F10,   KC_F11,   KC_F12,                        KC_1,     KC_2,     KC_3,     KC_DOT,
-              QK_BOOT,  KC_UNDS,  LCK_ALPM, KC_SLSH,  _______,   _______,  _______,  _______,  KC_COMM,  KC_PWR,   DB_TOGG
+              QK_BOOT,  KC_UNDS,  LCK_ALPM, KC_SLSH,  _______,   _______,  KC_COMM,  _______,  _______,  KC_PWR,   DB_TOGG
   ),
   [_SYMBOLS] = LAYOUT(
     _______,  KC_SPC,   KC_LPRN,  KC_QUOT,  KC_RPRN,  KC_EQL,    KC_COLN,  KC_AMPR,  KC_ASTR,  KC_QUES,  QK_LLCK,  _______,
     KC_TILD,  KC_SCLN,  KC_LBRC,  KC_DQUO,  KC_RBRC,  KC_MINS,   KC_0,     KC_DLR,   KC_PERC,  KC_CIRC,  KC_PLUS,  _______,
               KC_BSLS,  KC_LCBR,  KC_GRV,   KC_RCBR,                       KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
-              KC_VOLD,  KC_UNDS,  LCK_ALPM, KC_SLSH,  _______,   _______,  _______,  _______,  KC_COMM,  KC_VOLU,  DB_TOGG
+              KC_VOLD,  KC_UNDS,  LCK_ALPM, KC_SLSH,  _______,   _______,  KC_COMM,  _______,  _______,  KC_VOLU,  DB_TOGG
   ),
   [_ALP_OVL] = LAYOUT(
     _______,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     _______,
