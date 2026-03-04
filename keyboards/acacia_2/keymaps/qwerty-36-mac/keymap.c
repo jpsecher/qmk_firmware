7b2e1f2b621ae4a080d2534b363edb9de53e0e33
Jens Peter Secher <jpsecher@gmail.com>
2026-03-04 14:18:04 +0100

    agr on esc

--- keyboards/acacia_2/keymaps/qwerty-36-mac/keymap.c
+++ keyboards/acacia_2/keymaps/qwerty-36-mac/keymap.c
@@ -31,6 +31,7 @@ enum layer_names {
 #define MO_ALPM MO(_ALP_MAC)
 #define OSM_LSFT OSM(MOD_LSFT)
 #define ESC_SFT LSFT_T(KC_ESC)
+#define ESC_AGR RALT_T(KC_ESC)
 #define OSL_SYM OSL(_SYMBOLS)
 
 #define V_NUM LT(_NUM_MAC, KC_V)
@@ -80,9 +81,9 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALP_MAC] = LAYOUT(
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
-    TAB_FUNM, A_AGR,    S_ALT,    D_GUI,    F_CTL,    KC_G,      KC_H,     J_CTL,    K_GUI,    L_ALT,    B_AGR,    KC_ENT,
+    TAB_FUNM, KC_A,     S_ALT,    D_GUI,    F_CTL,    KC_G,      KC_H,     J_CTL,    K_GUI,    L_ALT,    KC_B,     KC_ENT,
               KC_Z,     KC_X,     KC_C,     V_NUM,                         N_NUM,    KC_M,     KC_COMM,  KC_DOT,
-              KC_MNXT,  ESC_SFT,  MO_NAV,   OSM_LSFT,                      OSL_SYM,  KC_SPC,   KC_BSPC,  KC_MPLY
+              KC_MNXT,  ESC_AGR,  MO_NAV,   OSM_LSFT,                      OSL_SYM,  KC_SPC,   KC_BSPC,  KC_MPLY
   ),
   [_NAV_MAC] = LAYOUT(
               OSL_ALPM, GUIY,     KC_WH_D,  KC_WH_U,  KC_BTN1,   CTLA,     KC_PGDN,  KC_PGUP,  CTLE,     QK_LLCK,
